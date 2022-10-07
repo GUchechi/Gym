b69e646260f14024bac67531ff4ecb45adddcdaa
GUchechi <godswilluchendu7@gmail.com>
2022-10-08 00:25:25 +0100

    worked on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
-    
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+import './Hero.css'
+const Hero = () => {
+
+  return (
+    <div className='hero'>
+      <div className='left-h'><Header /></div>
+      <div className='right-h'>Right</div>
+    </div>
+  )
+}
+
+export default Hero
