cedec27cb3d717ef14caa21b95de096f3e072d74
GUchechi <godswilluchendu7@gmail.com>
2022-10-13 16:20:50 +0100

    added react scroll

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ import {motion} from 'framer-motion'
 
 const Hero = () => {
   const transition = {type: 'spring', duration: 3}
+  const mobile = window.innerWidth <=768? true: false;
   return (
     <div className='hero'>
     <div className="blur hero-blur"></div>
@@ -18,7 +19,7 @@ const Hero = () => {
         {/* the best ad */}
         <div className="the-best-ad">
             <motion.div
-               initial={{left: '215px'}}
+               initial={{left: mobile?'165px' : '238px'}}
                whileInView ={{left: '8px'}}
                transition={{...transition, type: 'tween'}}
             >
